eea19afd45d8e4add591bf72f5e1374ac6d30878
kamausimon <kamausimon217@gmail.com>
2026-06-06 11:21:33 +0300

    implement user registration and login functionality with database integration

--- internal/config/commands.go
+++ internal/config/commands.go
@@ -1,12 +1,19 @@
 package config
 
 import (
+	"context"
+	"database/sql"
 	"fmt"
 	"os"
+	"time"
+
+	"github.com/Kamausimon/gator/internal/database"
+	"github.com/google/uuid"
 )
 
 type State struct {
 	Config *Config
+	Db     *database.Queries
 }
 
 type Command struct {
@@ -29,7 +36,44 @@ func HandlerLogin(s *State, cmd Command) error {
 	if err := s.Config.SetUser(username); err != nil {
 		return fmt.Errorf("failed to set user: %w", err)
 	}
+	_, err := s.Db.GetUserByName(context.Background(), username)
+	if err != nil {
+		fmt.Printf("user with name %s does not exist, please register first\n", username)
+		os.Exit(1)
+	}
 	fmt.Printf("username has been set successfully to %s\n", username)
+	return nil
+}
+func HandlerRegister(s *State, cmd Command) error {
+	if len(cmd.Arguments) < 1 {
+		if err := fmt.Errorf("name argument is required for register command"); err != nil {
+			fmt.Printf("error: %v\n", err)
+			os.Exit(1)
+		}
+	}
+	//create a new user in the database
+	name := cmd.Arguments[0]
+	ctx := context.Background()
+	_, err := s.Db.GetUserByName(ctx, name)
+	if err == nil {
+		fmt.Printf("user with name %s already exists", name)
+		os.Exit(1)
+	}
+	_, err = s.Db.CreateUser(ctx,
+		database.CreateUserParams{
+			ID:        uuid.New(),
+			CreatedAt: sql.NullTime{Time: time.Now(), Valid: true},
+			UpdatedAt: sql.NullTime{Time: time.Now(), Valid: true},
+			Name:      name,
+		})
+	if err != nil {
+		return fmt.Errorf("failed to create user: %w", err)
+	}
+	//set the current user in the config to the given name
+	if err := s.Config.SetUser(name); err != nil {
+		return fmt.Errorf("failed to set user: %w", err)
+	}
+	fmt.Printf("user created successfully: %s\n", name)
 	return nil
 }
 
--- main.go
+++ main.go
@@ -1,22 +1,34 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"os"
 
 	"github.com/Kamausimon/gator/internal/config"
+	"github.com/Kamausimon/gator/internal/database"
+	_ "github.com/lib/pq"
 )
 
 func main() {
+
 	cfg, err := config.Read()
 	if err != nil {
 		fmt.Printf("there was an error reading the config file %v", err)
 		return
 	}
-	appState := &config.State{Config: cfg}
+	db, err := sql.Open("postgres", os.Getenv("DBURL"))
+	if err != nil {
+		fmt.Printf("there was an error connecting to the database: %v\n", err)
+		return
+	}
+	defer db.Close()
+	dbQueries := database.New(db)
+	appState := &config.State{Config: cfg, Db: dbQueries}
 	cmds := &config.Commands{
 		Handlers: make(map[string]func(*config.State, config.Command) error)}
 	cmds.Register("login", config.HandlerLogin)
+	cmds.Register("register", config.HandlerRegister)
 	if len(os.Args) < 2 {
 		fmt.Println("Please provide a command")
 		os.Exit(1)
